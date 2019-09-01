07d081009418d257eb6d5411fc8a3c1e4a2a80ce
Davi Mello <davimello1994@gmail.com>
2019-08-31 23:15:30 -0400

    Part of logic added. Database finished and running

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,54 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '050994.Dm',
+    port: 3306,
+    database: 'bamazon_DB'
+});
+
+// Arrow function that holds errors
+
+connection.connect(err => {
+    if (err) throw err;
+    else console.log ("Connected to Bamazon Store!" + "\n" + "_____________________________" + "\n");
+    displayProducts();
+});
+
+function welcome() {
+    inquirer.prompt ({
+        name: "welcome",
+        type: "list",
+        message: "Welcome to Bamazon Store! What would you like to do today?",
+        choices: ["BUY", "EXIT"]
+    });
+
+    .then.
+}
+
+function displayProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+
+        for (i = 0; i < res.length; i++){
+        console.log("Item #" + res[i].id + " | " +
+                    "Name: " + res[i].product_name + "\n" +
+                    "Department: " + res[i].department_name + "\n" +
+                    "Price: " + res[i].price + "\n" + 
+                    "Quantity: " + res[i].stock_quantity + "\n");
+
+        }
+    })
+}
+
+// function checkout() {
+// inquirer.prompt ({
+//     name: "question",
+//     type: "list",
+//     message: "What product would you like to purchase?",
+//     choices: res[i].id
+// })
+// }
+// checkout()
--- bamazon_DB.sql
+++ bamazon_DB.sql
@@ -3,41 +3,40 @@ CREATE DATABASE bamazon_DB;
 USE bamazon_DB;
 
 CREATE TABLE products (
-  id INT NOT NULL AUTO_INCREMENT,
-  product_name VARCHAR(50) NULL,
-  department_name VARCHAR(50) NULL,
-  price DECIMAL(10,2) NULL,
-  stock_quantity INT default 50 NULL,
+  id INT(11) AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(150) NULL,
+  department_name VARCHAR(100) NULL,
+  price DECIMAL(65,2) NOT NULL,
+  stock_quantity INT(100) NOT NULL,
   PRIMARY KEY (id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("iPhone XS Max", "Electronics", "1000.99", "50");
+VALUES ("iPhone XS Max", "Electronics", 1000.99, 50);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Holy Bible", "Books", "20.39", "100");
+VALUES ("Holy Bible", "Books", 20.39, 100);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("H&M Crewneck Sweatshirt", "Clothing", "19.99", "100");
+VALUES ("H&M Crewneck Sweatshirt", "Clothing", 19.99, 100);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Classic Office Chair", "Home Office", "59.79", "50");
+VALUES ("Classic Office Chair", "Home Office", 59.79, 50);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Nest Thermostat", "Smart Home", "120.10", "75");
+VALUES ("Nest Thermostat", "Smart Home", 120.19, 75);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Starbucks Pike Place 20oz", "Grocery", "7.89", "50");
+VALUES ("Starbucks Pike Place 20oz", "Grocery", 7.89, 50);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Pedigree - Roasted Chicken Flavor 70oz", "Pet Supplies", "22.10", "25");
+VALUES ("Pedigree Chicken Flavor 70oz", "Pet Supplies", 22.10, 25);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Buzz Lightyear Original Size", "Toys", "60.90", "70");
+VALUES ("Buzz Lightyear Original Size", "Toys", 60.90, 70);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Barcelona Football Club Official Jersey", "Sports", "110.80", "30");
+VALUES ("Barcelona Football Club Official Jersey", "Sports", 110.80, 30);
 
-SELECT * FROM products;
-
-SHOW TABLES;
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Vitamins Leave Cream 10oz", "Beauty & Health", 18.10, 5);
