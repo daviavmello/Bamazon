d289789f33943254bc1d94bdad0a7ddad96fe4bf
Davi Mello <davimello1994@gmail.com>
2019-09-01 23:13:34 -0400

    Added .gitignore and troubleshooting

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,26 +13,92 @@ var connection = mysql.createConnection({
 
 connection.connect(err => {
     if (err) throw err;
-    else console.log ("Connected to Bamazon Store!" + "\n" + "_____________________________" + "\n");
-    displayProducts();
+    else console.log ("You're connected!" + "\n" + "_____________________________" + "\n");
 });
 
-function welcome() {
-    inquirer.prompt ({
+displayProducts();
+
+inquirer.prompt ([ 
+        {
         name: "welcome",
         type: "list",
         message: "Welcome to Bamazon Store! What would you like to do today?",
         choices: ["BUY", "EXIT"]
+        }
+
+    ]).then(function (answer) {
+        connection.query("SELECT * FROM products", function (err,res) {
+            if (answer.welcome === "EXIT") {
+                console.log("\nSee you later!")
+            }
+            else {
+                selectProduct();
+            }
+        })
     });
 
-    .then.
+function selectProduct() {
+
+inquirer.prompt ([
+    {
+        name: "chooseProduct",
+        type: "input",
+        message: "Awesome! What product would you like to purchase today? Please type the id number.",
+        validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+        },
+
+        {
+        name: "quantity",
+        type: "input",
+        message: "Great choice! How many would you like to get?",
+        validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+        }
+    ]).then(function(answer) {
+        connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        var chosenItem;
+        for (var i = 0; i < res.length; i++) {
+            if (res[i].id === parseInt(answer.chooseProduct)) {
+                chosenItem = res[i];
+            }
+        }   
+            console.log("\nYou have chosen to purchase " + answer.quantity + " unit(s) of the " + chosenItem.product_name);       
+});
+
+    if (chosenItem.stock_quantity > parseInt(answer.quantity)) {
+        connection.query ("UPDATE products SET ? WHERE ?")
+        [
+            {
+                id: chosenItem.id
+            },
+            {
+                stock_quantity: chosenItem.stock_quantity - parseInt(answer.quantity)
+            }
+        ]
+    }
+
+    else {
+        console.log("Too bad! The item you chose only has " + chosenItem.stock_quantity + " availabe")
+    }
+    
+});
 }
 
 function displayProducts() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
 
-        for (i = 0; i < res.length; i++){
+        for (i = 0; i < res.length; i++) {
         console.log("Item #" + res[i].id + " | " +
                     "Name: " + res[i].product_name + "\n" +
                     "Department: " + res[i].department_name + "\n" +
@@ -42,13 +108,3 @@ function displayProducts() {
         }
     })
 }
-
-// function checkout() {
-// inquirer.prompt ({
-//     name: "question",
-//     type: "list",
-//     message: "What product would you like to purchase?",
-//     choices: res[i].id
-// })
-// }
-// checkout()
